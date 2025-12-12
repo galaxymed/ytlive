6a28119dae73cffe31dce3acdfa56857376ce830
galaxymed <163075796+galaxymed@users.noreply.github.com>
2025-12-12 00:34:10 -0300

    Update handler to use async and redirect to manifest
    Refactor handler to redirect to HLS manifest URL.

--- api/channel/[id].js
+++ api/channel/[id].js
@@ -1,18 +1,14 @@
-export default function handler(req, res) {
+export default async function handler(req, res) {
   const { id } = req.query;
 
   if (!id) {
     return res.status(400).send("Missing channel ID");
   }
 
-  const hlsUrl = `https://manifest.googlevideo.com/api/manifest/hls_variant/expire/9999999/id/${id}.m3u8`;
+  // URL del manifest HLS de YouTube
+  const manifestUrl = `https://manifest.googlevideo.com/api/manifest/hls_variant/expire/9999999/id/${id}.m3u8`;
 
-  const playlist = `#EXTM3U
-#EXT-X-VERSION:3
-#EXT-X-STREAM-INF:BANDWIDTH=2560000,RESOLUTION=1280x720
-${hlsUrl}
-`;
-
-  res.setHeader("Content-Type", "application/vnd.apple.mpegurl");
-  res.status(200).send(playlist);
+  // Redirigir al cliente al manifest real
+  res.writeHead(302, { Location: manifestUrl });
+  res.end();
 }
